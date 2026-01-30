c1d7a7d06b4096d6eda4469499ac16141e0dba96
philipp <philipp@steckenbauer.com>
2026-01-30 15:33:21 +0700

    Added Inference Helpers

--- Schema/src/util/index.ts
+++ Schema/src/util/index.ts
@@ -1,4 +1,4 @@
-import type {Validator} from '../validators/Validator';
+import {Validator} from '../validators/Validator';
 
 export type ApplyNullability<T, N extends boolean> =
 	N extends true ? T | null : Exclude<T, null>;
@@ -7,9 +7,22 @@ export interface Definition {
 	type: string;
 }
 
+export type InferDefinitionType<V> = V extends { toJSON(): infer D } ? D : never;
 export type InferValidatorReturnType<V> = V extends Validator<infer T> ? T : unknown;
 
-export type InferDefinitionType<V> = V extends { toJSON(): infer D } ? D : never;
+export type InferListDefinitionType<T extends readonly Validator[]> = {
+	[K in keyof T]: InferDefinitionType<T[K]>;
+};
+export type InferListReturnType<T> = T extends readonly Validator[] ?
+	{ [K in keyof T]: InferValidatorReturnType<T[K]> } :
+	unknown;
+
+export type InferObjectDefinitionType<T extends { [key: string]: Validator }> = {
+	[K in keyof T]: InferDefinitionType<T[K]>
+}
+export type InferObjectReturnType<T> = T extends { [key: string]: Validator } ?
+	{ [K in keyof T]: InferValidatorReturnType<T[K]> } :
+	unknown;
 
 export class AssertError extends Error {}
 
