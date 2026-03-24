cc484a7827a34200d2204658f4d48848fcf18953
philipp <philipp@steckenbauer.com>
2026-03-24 23:57:04 +0100

    Added Builder and Validator indices

---
+++ Schema/src/Builders/index.ts
@@ -0,0 +1,7 @@
+export {JSONBuilder} from './JSON';
+export {BooleanBuilder} from './Boolean';
+export {NumberBuilder} from './Number';
+export {StringBuilder} from './String';
+export {ArrayBuilder} from './Array';
+export {ObjectBuilder} from './Object';
+export {UnionBuilder} from './Union';
---
+++ Schema/src/Validators/index.ts
@@ -0,0 +1,7 @@
+export {JSONValidator} from './JSON';
+export {BooleanValidator} from './Boolean';
+export {NumberValidator} from './Number';
+export {StringValidator} from './String';
+export {ArrayValidator} from './Array';
+export {ObjectValidator} from './Object';
+export {UnionValidator} from './Union';
