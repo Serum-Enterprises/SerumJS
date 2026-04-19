92fa8cb7f37baa88aafa177c57342e9c23865669
philipp <philipp@steckenbauer.com>
2026-04-19 10:59:43 +0700

    Removed Builders

--- Schema/src/Builders/JSON.ts
+++
@@ -1,4 +0,0 @@
-import {JSON} from '@serum-enterprises/json';
-import {JSONValidator} from '../Validators/JSON';
-
-export class JSONBuilder<T extends JSON.JSON = JSON.JSON> extends JSONValidator<T> {}
--- Schema/src/Builders/index.ts
+++
@@ -1,6 +0,0 @@
-export {JSONBuilder} from './JSON';
-export {BooleanBuilder} from './Boolean';
-export {NumberBuilder} from './Number';
-export {StringBuilder} from './String';
-export {ArrayBuilder} from './Array';
-export {ObjectBuilder} from './Object';
