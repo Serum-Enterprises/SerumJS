ac77957632d892cb8ad33b97f270e739aac8fca1
philipp <philipp@steckenbauer.com>
2026-04-19 11:19:21 +0700

    Added Tests for Boolean

---
+++ Schema/tests/validators/Boolean.test.ts
@@ -0,0 +1,32 @@
+import {SchemaDomain, AssertError, BooleanValidator, DefinitionError} from '../../src';
+
+const Schema = SchemaDomain.create();
+
+describe('Testing Boolean Validator', () => {
+	test('Testing assert', () => {
+		expect(() => Schema.Boolean.assert(true)).not.toThrow();
+		expect(() => Schema.Boolean.assert(false)).not.toThrow();
+
+		expect(() => Schema.Boolean.assert(null)).toThrow(AssertError);
+		expect(() => Schema.Boolean.assert(1)).toThrow(AssertError);
+		expect(() => Schema.Boolean.assert('Hello World')).toThrow(AssertError);
+
+		expect(() => Schema.Boolean.equals(true).assert(true)).not.toThrow();
+		expect(() => Schema.Boolean.equals(false).assert(true)).toThrow(AssertError);
+	});
+
+	test('Testing equals', () => {
+		expect(Schema.Boolean.equals(true)).toBeInstanceOf(BooleanValidator)
+	})
+
+	test('Testing toJSON', () => {
+		expect(Schema.Boolean.toJSON()).toEqual({type: 'boolean'});
+		expect(Schema.Boolean.equals(true).toJSON()).toEqual({type: 'boolean', equals: true});
+	})
+
+	test('Testing fromJSON', () => {
+		expect(Schema.fromJSON({type: 'boolean'})).toBeInstanceOf(BooleanValidator);
+		expect(Schema.fromJSON({type: 'boolean', equals: true})).toBeInstanceOf(BooleanValidator);
+		expect(Schema.fromJSON({type: 'boolean', equals: 'Hello World'})).toBeInstanceOf(DefinitionError);
+	});
+});
